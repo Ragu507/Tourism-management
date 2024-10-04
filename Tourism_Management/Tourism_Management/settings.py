ab360671f5ec58e11703ced36905711a27ab5f2c
Ragu507 <162023493+Ragu507@users.noreply.github.com>
2024-10-04 20:55:29 +0530

    Update settings.py

--- Tourism_Management/Tourism_Management/settings.py
+++ Tourism_Management/Tourism_Management/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,9 +76,10 @@ WSGI_APPLICATION = 'Tourism_Management.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
